2498a44e30dd73875f89f3e231bdafd6ec97a2f0
aakarshan-raj <aakarshan7raj@gmail.com>
2023-12-20 21:53:39 +0530

    added npm for types

--- Lexer/lexer.js
+++ Lexer/lexer.js
@@ -1,16 +1,61 @@
-// let x = 4 + (4*5)
+var fs = require("fs");
 var Type;
 (function (Type) {
-    Type[Type["Number"] = 0] = "Number";
-    Type[Type["Let"] = 1] = "Let";
-    Type[Type["Identifier"] = 2] = "Identifier";
+    Type[Type["Equal"] = 0] = "Equal";
+    Type[Type["Colon"] = 1] = "Colon";
+    Type[Type["BinaryOperator"] = 2] = "BinaryOperator";
     Type[Type["CloseParen"] = 3] = "CloseParen";
     Type[Type["OpenParen"] = 4] = "OpenParen";
-    Type[Type["BinaryOperator"] = 5] = "BinaryOperator";
-    Type[Type["WhiteSpace"] = 6] = "WhiteSpace";
-    Type[Type["Colon"] = 7] = "Colon";
+    Type[Type["Number"] = 5] = "Number";
+    Type[Type["Let"] = 6] = "Let";
+    Type[Type["Identifier"] = 7] = "Identifier";
+    Type[Type["WhiteSpace"] = 8] = "WhiteSpace";
 })(Type || (Type = {}));
-function Lexer(src) {
-    console.log(src);
+function Fill(value, type) {
+    var t = { value: value, type: type };
+    return t;
 }
-Lexer("hi");
+function range(a, b, c) {
+    var bound = [a.charCodeAt(0), b.charCodeAt(0)];
+    var target = c.charCodeAt(0);
+    if (target >= bound[0] && target <= bound[1]) {
+        return true;
+    }
+    return false;
+}
+function isAlpha(c) {
+    return range("a", "b", c);
+}
+function isNum(c) {
+    return range("0", "9", c);
+}
+function Lexer(src) {
+    var token = Array();
+    var source = src.split("");
+    while (source.length > 0) {
+        if (source[0] == "=") {
+            token.push(Fill(source.shift(), Type.Equal));
+        }
+        else if (source[0] == "(") {
+            token.push(Fill(source.shift(), Type.OpenParen));
+        }
+        else if (source[0] == ")") {
+            token.push(Fill(source.shift(), Type.CloseParen));
+        }
+        else if (source[0] == "*" || source[0] == "/" || source[0] == "+" || source[0] == "-") {
+            token.push(Fill(source.shift(), Type.BinaryOperator));
+        }
+        else {
+            console.log("This is not yet covered:" + source.shift());
+        }
+    }
+    return token;
+}
+var code = fs.readFileSync("code.tl", "utf-8");
+var data = Lexer(code);
+for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
+    var x = data_1[_i];
+    console.log(x);
+}
+var a = '1';
+console.log(isNum(a));
--- Lexer/lexer.ts
+++ Lexer/lexer.ts
@@ -1,24 +1,73 @@
-// let x = 4 + (4*5)
+const fs = require("fs");
 
-
-
-enum Type{
-   Number,
-   Let,
-   Identifier,
-   CloseParen,
-   OpenParen,
-   BinaryOperator,
-   WhiteSpace,
-   Colon,
+enum Type {
+    Equal,
+    Colon,
+    BinaryOperator,
+    CloseParen,
+    OpenParen,
+    Number,
+    Let,
+    Identifier,
+    WhiteSpace,
 }
-interface Token{
-    value:String,
-    type:Type
-}
-
-function Lexer(src:string){
-    console.log(src);
+interface Token {
+    value: string,
+    type: Type
 }
 
-Lexer("hi");
+function Fill(value: string, type: Type) {
+    const t: Token = { value: value, type: type };
+    return t;
+}
+
+function range(a: string, b: string, c: string): boolean {
+    const bound = [a.charCodeAt(0), b.charCodeAt(0)];
+    const target = c.charCodeAt(0);
+    if (target >= bound[0] && target <= bound[1]) {
+        return true;
+    }
+    return false;
+}
+
+function isAlpha(c: string) {
+    return range("a", "b", c);
+}
+
+
+function isNum(c: string) {
+    return range("0", "9", c);
+}
+
+function Lexer(src: string): Token[] {
+    const token = Array<Token>();
+    const source = src.split("");
+    while (source.length > 0) {
+        if (source[0] == "=") {
+            token.push(Fill(source.shift(), Type.Equal));
+        }
+        else if (source[0] == "(") {
+            token.push(Fill(source.shift(), Type.OpenParen));
+        }
+        else if (source[0] == ")") {
+            token.push(Fill(source.shift(), Type.CloseParen));
+        }
+        else if (source[0] == "*" || source[0] == "/" || source[0] == "+" || source[0] == "-") {
+            token.push(Fill(source.shift(), Type.BinaryOperator));
+        }
+        else {
+            console.log("This is not yet covered:" + source.shift());
+        }
+    }
+    return token;
+}
+
+const code = fs.readFileSync("code.tl","utf-8");
+
+const data = Lexer(code);
+for (const x of data){
+    console.log(x);
+}
+
+const a = '1';
+console.log(isNum(a));
