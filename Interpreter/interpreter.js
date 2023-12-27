657b31fbcde92e8e8ca934de408f7cb3ad129bd2
aakarshan-raj <aakarshan7raj@gmail.com>
2023-12-27 15:58:03 +0530

    evaluating basic binary expression, the wrong way

--- Interpreter/interpreter.js
+++ Interpreter/interpreter.js
@@ -1,12 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Interpreter = void 0;
+function evaluate_binary_expression(x) {
+    var a = x.left.value;
+    var b = x.right.value;
+    var answer = 0;
+    if (x.operator == "+") {
+        answer = a + b;
+    }
+    if (x.operator == "-") {
+        answer = a - b;
+    }
+    return { type: 'number', value: answer };
+}
 function Interpreter(astNode) {
     switch (astNode.kind) {
         case "NUMERICAL_LITERAL":
             return { type: 'number', value: astNode.value };
         case "NULL":
             return { type: 'null', value: "null" };
+        case "BINARY_EXPRESSION":
+            return evaluate_binary_expression(astNode);
         default:
             console.log("Not yet interpreting: " + astNode.kind);
     }
--- Interpreter/interpreter.ts
+++ Interpreter/interpreter.ts
@@ -1,13 +1,30 @@
-import { NullLiteral, NumericalLiteral, Statement } from "../AST/ast";
+import { BinaryExpression, NullLiteral, NumericalLiteral, Statement } from "../AST/ast";
 import { NullValue, NumercialValue, RunTime } from "./values";
+
+
+function evaluate_binary_expression(x: BinaryExpression): RunTime {
+    const a = (x.left as NumericalLiteral).value;
+    const b = (x.right as NumericalLiteral).value;
+    let answer = 0;
+    if (x.operator == "+") {
+        answer = a + b;
+    }
+    if (x.operator == "-") {
+        answer = a - b;
+    }
+    return {type:'number', value:answer} as NumercialValue
+}
+
 
 export function Interpreter(astNode: Statement): RunTime {
     switch (astNode.kind) {
         case "NUMERICAL_LITERAL":
             return { type: 'number', value: (astNode as NumericalLiteral).value } as NumercialValue;
         case "NULL":
-            return {type:'null', value:"null"} as NullValue;
+            return { type: 'null', value: "null" } as NullValue;
+        case "BINARY_EXPRESSION":
+            return evaluate_binary_expression(astNode as BinaryExpression);
         default:
-            console.log("Not yet interpreting: "+astNode.kind);
+            console.log("Not yet interpreting: " + astNode.kind);
     }
 }
